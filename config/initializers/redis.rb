586e9ce1e62ed2cae41258486700614b606cc279
Fabio Maggi <fabio.maggi@alfaconcursos.com.br>
2021-01-26 17:33:10 -0300

    fix conflict redis dev-prod

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+if Rails.env.production?
+  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+else
+  Sidekiq.configure_server do |config|
+    config.redis = { password: 'Redis2019!' }
+  end
+  
+  Sidekiq.configure_client do |config|
+    config.redis = { password: 'Redis2019!' }
+  end
+end
